bc44f65281e8ea43106667184b940fa8385a1069
sum182 <sum182@gmail.com>
2022-03-15 11:17:27 -0300

    add files

---
+++ Alura/bytebank/src/bytebank/Conta.java
@@ -0,0 +1,23 @@
+package bytebank;
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String Titular;
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+}
---
+++ IDE-Testes/goca-vscode/src/main/java/br/com/goca/App.java
@@ -0,0 +1,18 @@
+package br.com.goca;
+//import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        
+       // get("goca-vscode", (req,res) -> "vs-code!");
+        System.out.println( "Hello World!" );
+
+    }
+}
