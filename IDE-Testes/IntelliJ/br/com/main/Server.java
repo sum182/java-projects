c2b8ecb86a3c9e957b70a5ffb3ae1740dca5fcdd
sum182 <sum182@gmail.com>
2022-03-15 11:04:05 -0300

    add files

---
+++ IDE-Testes/IntelliJ/br/com/main/Server.java
@@ -0,0 +1,13 @@
+package br.com.main;
+
+//import static spark.Spark.*;
+
+
+public class Server {
+
+    public static void main(String [] args){
+        //get(pat)
+        System.out.println("teste");
+    }
+
+}
