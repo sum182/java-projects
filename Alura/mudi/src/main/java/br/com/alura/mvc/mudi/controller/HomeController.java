b39c8de903f66688a8c5bc2d0828e78c41385ff0
Álvaro Esteves <sum182@gmail.com>
2022-03-27 18:14:23 -0300

    .

---
+++ Alura/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,28 @@
+package br.com.alura.mvc.mudi.controller;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("/home")
+    public String home(Model model){
+
+      List<Pedido> pedidos = pedidoRepository.findAll();
+      model.addAttribute("pedidos", pedidos);
+
+      return "home";
+
+    }
+
+
+}
