55fd733c3cbe75fe453a379d5ac0627a949a62ad
Álvaro Esteves <sum182@gmail.com>
2022-04-09 01:19:49 -0300

    aula 3

--- Alura/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ Alura/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -18,14 +18,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .antMatchers("/home/**")
+        .permitAll()
         .anyRequest()
         .authenticated()
         .and()
         .formLogin(
             form -> form.loginPage("/login").defaultSuccessUrl("/usuario/pedido", true).permitAll())
-        .logout(logout -> logout.logoutUrl("/logout"))
-        .csrf()
-        .disable();
+        .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/home"));
   }
 
   @Override
