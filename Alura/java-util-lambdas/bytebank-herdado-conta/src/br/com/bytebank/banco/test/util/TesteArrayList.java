58dbe616a40bfd3cce7b500167cb01cb8fbbeac7
Álvaro Esteves <sum182@gmail.com>
2022-04-30 15:55:31 -0300

    .

--- Alura/java-util-lambdas/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayList.java
+++ Alura/java-util-lambdas/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayList.java
@@ -4,7 +4,7 @@ import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import java.util.ArrayList;
 
-public class Teste {
+public class TesteArrayList {
 
   public static void main(String[] args) {
     //<> Generics
