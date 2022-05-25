60ff2cd2efee3ae416682e64337afdf872706c33
Álvaro Esteves <sum182@gmail.com>
2022-05-25 12:19:09 -0300

    Swagger and Log Formart

--- Alura/mudi/src/main/java/br/com/alura/mvc/mudi/WebConfig.java
+++ Alura/mudi/src/main/java/br/com/alura/mvc/mudi/WebConfig.java
@@ -5,12 +5,28 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import br.com.alura.mvc.mudi.interceptor.InterceptadorDeAcessos;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+devemos utilizar WebMvcConfigurer ao inves de WebMvcConfigurationSupport no webConfig
+a WebMvcConfigurationSupport foi atualizada para WebMvcConfigurer
+
+isso serve para rodar o Swagger
 
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
 
   @Override
   protected void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new InterceptadorDeAcessos()).addPathPatterns("/**");
+  }
+*/
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new InterceptadorDeAcessos()).addPathPatterns("/**");
   }
 }
